ab7e90dc9b388cd48da3379652d9700e03af8eb9
YuliaKorchahina <ruda_nokia@ukr.net>
2023-01-16 22:28:32 +0200

    simpleligthbox/style

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,7 +1,9 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api';
 const KEY = '32745455-50ea65fac97b0fc063f154f63';
 
-export default function fetchData(searchQuery, page = 1, perPage = 40) {
+export default async function fetchData(searchQuery, page = 1, perPage = 40) {
   // const options = {
   //   image_type: 'photo',
   //   orientation: 'horizontal',
@@ -10,16 +12,10 @@ export default function fetchData(searchQuery, page = 1, perPage = 40) {
   // };
   // return fetch(`${BASE_URL}?key=${KEY}&q=${searchQuery}`, options)
 
-  return fetch(
-    `${BASE_URL}/?key=${KEY}&image_type=photo&page=${page}&per_page=${perPage}&orientation=horizontal&safesearch=true&q=${searchQuery}`
-  )
-    .then(resp => {
-      if (!resp.ok) {
-        throw new Error(resp.statusText);
-      }
-      return resp.json();
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
+  return await axios
+    .get(
+      `${BASE_URL}/?key=${KEY}&image_type=photo&page=${page}&per_page=${perPage}&orientation=horizontal&safesearch=true&q=${searchQuery}`
+    )
+    .then(resp => resp.data);
+
+ }
--- src/index.js
+++ src/index.js
@@ -8,21 +8,16 @@ const input = document.querySelector('input[name="searchQuery"]');
 const gallery = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
 const info = document.querySelector('.info');
-//const cardContainer = document.querySelector('.photo-card')
 
-form.style.margin = '30px';
 gallery.style.display = 'flex';
 gallery.style.gap = '30px';
 gallery.style.flexWrap = 'wrap';
-//info.style.display= 'flex';
-//form.style.color = 'pink'
 
-//btnLoadMore.disabled = true;
 btnLoadMore.style.display = 'none';
 
 let searchQuery = '';
 let page = 1;
-let perPage = 200;
+let perPage = 40;
 let currentData = [];
 
 const addData = searchData => {
@@ -33,25 +28,31 @@ const handleInput = e => {
   searchQuery = e.currentTarget.value;
 };
 
-const handleSubmit = event => {
+async function handleSubmit(event) {
   event.preventDefault();
-  if (searchQuery) {
-    btnLoadMore.style.display = 'block';
-    fetchData(`${searchQuery}`).then(data => {
-      const { hits,totalHits } = data;
+  currentData = [];
+  btnLoadMore.style.display = 'block';
+  try {
+    const resp = await fetchData(`${searchQuery}`);
+    if (searchQuery) {
+      const { hits, totalHits } = resp;
       if (!hits.length) {
         Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
+        gallery.innerHTML = '';
+        btnLoadMore.style.display = 'none';
         return;
       }
-      renderList(hits);
-      addData(hits);
       Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
-      console.log(currentData);
-    });
+      renderList(hits);
+      lightbox.refresh();
+      addData(hits);
+    }
+  } catch (error) {
+    console.log(error);
   }
-};
+}
 
 function renderList(data) {
   const markup = data
@@ -91,43 +92,32 @@ function renderList(data) {
   gallery.innerHTML = markup;
 }
 
-function handleLoadMore(event) {
+async function handleLoadMore(event) {
   event.preventDefault();
   page += 1;
   console.log(page);
-  fetchData(`${searchQuery}`, page, 200)
-    .then(data => {
-      const { hits, totalHits, total } = data;
-      addData(hits);
-      renderList(currentData);
-      console.log(total);
-      console.log(currentData.length);
-
-      const totalElementsForPage = page * perPage;
-      console.log(totalElementsForPage);
-      // if (totalElementsForPage > 420)
-      if (data.page === data.per_page) {
-        console.log(data.page);
-        console.log(data.per_page);
-        btnLoadMore.style.display = 'none';
-        setTimeout(() => {
-          Notiflix.Notify.failure(
-            "We're sorry, but you've reached the end of search results."
-          );
-        }, 2000);
-      }
-    })
-    .catch(err => console.error(err));
+  const resp = await fetchData(`${searchQuery}`, page);
+  const { hits, totalHits, total } = resp;
+  addData(hits);
+  renderList(currentData);
   lightbox.refresh();
+  const totalElementsForPage = page * perPage;
+  console.log(totalElementsForPage);
+  if (totalElementsForPage > totalHits) {
+    //if (resp.page === resp.per_page) {
+    // console.log(data.page);
+    // console.log(data.per_page);
+    btnLoadMore.style.display = 'none';
+    setTimeout(() => {
+      Notiflix.Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }, 1000);
+  }
 }
-
-const onClick = e => {
-  e.preventDefault();
-};
 
 let lightbox = new SimpleLightbox('.gallery a');
 
 input.addEventListener('input', handleInput);
 form.addEventListener('submit', handleSubmit);
 btnLoadMore.addEventListener('click', handleLoadMore);
-//gallery.addEventListener('click', onClick);
